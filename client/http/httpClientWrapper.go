80a98dc16a912d5e9fb055dd6c90a4e9b7518de5
Stav Gayer <stav.g@heilasystems.com>
2021-06-01 15:13:44 +0300

    transport now supprt custom marshal and override content type

--- client/http/httpClientWrapper.go
+++ client/http/httpClientWrapper.go
@@ -7,8 +7,8 @@ import (
 	"bitbucket.org/HeilaSystems/transport/client"
 	"bytes"
 	"context"
-	jsoniter "github.com/json-iterator/go"
 	"fmt"
+	jsoniter "github.com/json-iterator/go"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -17,78 +17,80 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 type httpClientWrapper struct {
 	client *http.Client
-	conf configuration.Config
+	conf   configuration.Config
 }
 
-func (h *httpClientWrapper) Call(c context.Context,payload interface{},host, handler string,target interface{},headers map[string]string) ServiceReply {
-	return h.do(c,http.MethodPost,payload,host,handler,target,headers,true)
+func (h *httpClientWrapper) Call(c context.Context, payload interface{}, host, handler string, target interface{}, headers map[string]string) ServiceReply {
+	return h.do(c, http.MethodPost, payload, host, handler, target, headers, true)
 }
 
-func (h *httpClientWrapper) Post(c context.Context,payload interface{},host, handler string,target interface{},headers map[string]string) ServiceReply {
-	return h.do(c,http.MethodPost,payload,host,handler,target,headers,false)
+func (h *httpClientWrapper) Post(c context.Context, payload interface{}, host, handler string, target interface{}, headers map[string]string) ServiceReply {
+	return h.do(c, http.MethodPost, payload, host, handler, target, headers, false)
 }
 
-func (h *httpClientWrapper) Get(c context.Context,host, handler string,target interface{},headers map[string]string)  ServiceReply  {
-	return h.do(c,http.MethodGet,nil,host,handler,target,headers,false)
+func (h *httpClientWrapper) Get(c context.Context, host, handler string, target interface{}, headers map[string]string) ServiceReply {
+	return h.do(c, http.MethodGet, nil, host, handler, target, headers, false)
 }
 
-func (h *httpClientWrapper) Put(c context.Context,payload interface{},host, handler string,target interface{},headers map[string]string)  ServiceReply  {
-	return h.do(c,http.MethodPut,payload,host,handler,target,headers,false)
+func (h *httpClientWrapper) Put(c context.Context, payload interface{}, host, handler string, target interface{}, headers map[string]string) ServiceReply {
+	return h.do(c, http.MethodPut, payload, host, handler, target, headers, false)
 }
 
-func (h *httpClientWrapper) Delete(c context.Context,host, handler string,target interface{},headers map[string]string) ServiceReply  {
-	return h.do(c,http.MethodDelete,nil,host,handler,target,headers,false)
+func (h *httpClientWrapper) Delete(c context.Context, host, handler string, target interface{}, headers map[string]string) ServiceReply {
+	return h.do(c, http.MethodDelete, nil, host, handler, target, headers, false)
 }
 
-func NewHttpClientWrapper(client *http.Client ,conf  configuration.Config) (client.HttpClient,error) {
-	return &httpClientWrapper{client: client, conf: conf},nil
+func NewHttpClientWrapper(client *http.Client, conf configuration.Config) (client.HttpClient, error) {
+	return &httpClientWrapper{client: client, conf: conf}, nil
 }
 
-func (h *httpClientWrapper) do(c context.Context,httpMethod string ,payload interface{},host, handler string,target interface{},headers map[string]string,internal bool) (srvReply ServiceReply)  {
+func (h *httpClientWrapper) do(c context.Context, httpMethod string, payload interface{}, host, handler string, target interface{}, headers map[string]string, internal bool) (srvReply ServiceReply) {
 	url := fmt.Sprintf("http://%s/%s", host, handler)
-	if overrideHost := os.Getenv(host+urlKeyword); len(overrideHost) > 0 {
-		url = fmt.Sprintf("%s/%s" , overrideHost, handler)
+	if overrideHost := os.Getenv(host + urlKeyword); len(overrideHost) > 0 {
+		url = fmt.Sprintf("%s/%s", overrideHost, handler)
 	}
 	srvReply = NewNil()
-	b , sErr := getPayload(payload , url)
-	if sErr != nil{
+	b, sErr := getPayload(payload, url)
+	if sErr != nil {
 		return sErr
 	}
 	var req *http.Request
 	var err error
 	if b != nil {
-		req, err = http.NewRequest(httpMethod,url,  b)
+		req, err = http.NewRequest(httpMethod, url, b)
 	} else {
-		req, err = http.NewRequest(httpMethod,url,  nil)
+		req, err = http.NewRequest(httpMethod, url, nil)
 	}
 	if err != nil {
-		return NewInternalServiceError( err).WithLogMessage(fmt.Sprintf("Cannot marshal request to %s" , url))
+		return NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("Cannot marshal request to %s", url))
 	}
 	req = req.WithContext(c)
-	req.Header.Add("Content-Type", "application/json")
 	for key, value := range headers {
 		req.Header.Add(key, value)
+	}
+	if _ , ok := headers["Content-Type"]; !ok {
+		req.Header.Add("Content-Type", "application/json")
 	}
 
 	resp, err := h.client.Do(req)
 	if err != nil {
-		return NewIoError(err).WithLogMessage(fmt.Sprintf("couldn't send %s request to %s" , httpMethod,url))
+		return NewIoError(err).WithLogMessage(fmt.Sprintf("couldn't send %s request to %s", httpMethod, url))
 	}
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return NewInternalServiceError( err).WithLogMessage(fmt.Sprintf("cannot read response from %s",url) )
+		return NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot read response from %s", url))
 	}
 	if internal {
 		var srvError Response
 		if err := json.Unmarshal(body, &srvError); err != nil {
-			return NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot read response from %s",url)).WithLogValues(ValuesMap{"rawResponse" : string(body)})
+			return NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot read response from %s", url)).WithLogValues(ValuesMap{"rawResponse": string(body)})
 		}
 		if srvError.Status != status.SuccessStatus {
 			resType := status.GetTypeByStatus(srvError.GetStatus())
 			msgValues := srvError.GetMessageValues()
-			srvReply = NewServiceError( &resType, nil,srvError.GetMessageId(),1 )
+			srvReply = NewServiceError(&resType, nil, srvError.GetMessageId(), 1)
 			if msgValues != nil {
 				srvReply = srvReply.WithReplyValues(*msgValues)
 			}
@@ -102,42 +104,52 @@ func (h *httpClientWrapper) do(c context.Context,httpMethod string ,payload inte
 			}
 		}
 		if srvError.Data != nil {
-			if dataJson  ,err := json.Marshal(srvError.Data);err != nil {
-				return NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot marshal data from %s",url))
-			}else {
+			if dataJson, err := json.Marshal(srvError.Data); err != nil {
+				return NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot marshal data from %s", url))
+			} else {
 				body = dataJson
 			}
 		} else {
 			body = nil
 		}
 	}
-	if err := unmarshalDataToStruct(body , target,url);err != nil {
+	if err := unmarshalDataToStruct(body, target, url); err != nil {
 		return err
 	}
 	return
 }
 
-func unmarshalDataToStruct(data []byte,target interface{},logStrings ...interface{}) ServiceReply  {
-	if target == nil{
+type Marshaler interface {
+	Unmarshal(data []byte) error
+}
+
+func unmarshalDataToStruct(data []byte, target interface{}, logStrings ...interface{}) ServiceReply {
+	if target == nil {
 		return nil
 	}
 	if data == nil {
-		return NewInternalServiceError(nil).WithLogMessage(fmt.Sprintf("Cannot unmarshal empty response from %s to target struct",logStrings...))
+		return NewInternalServiceError(nil).WithLogMessage(fmt.Sprintf("Cannot unmarshal empty response from %s to target struct", logStrings...))
+	}
+	if f,ok := target.(Marshaler);ok {
+		if err := f.Unmarshal(data);err != nil {
+			return NewInternalServiceError(err)
+		}
+		return nil
 	}
 	if err := json.Unmarshal(data, &target); err != nil {
 		return NewInternalServiceError(err).WithLogMessage("cannot read response")
 	}
 	return nil
 }
-func getPayload(payload interface{},url string) (*bytes.Buffer,ServiceReply) {
+func getPayload(payload interface{}, url string) (*bytes.Buffer, ServiceReply) {
 	if payload != nil {
 		request, err := json.Marshal(payload)
 		if err != nil {
-			return nil, NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot read response from %s",url)).WithLogValues(ValuesMap{"rawResponse" : payload})
+			return nil, NewInternalServiceError(err).WithLogMessage(fmt.Sprintf("cannot read response from %s", url)).WithLogValues(ValuesMap{"rawResponse": payload})
 		}
-		return bytes.NewBuffer(request) , nil
+		return bytes.NewBuffer(request), nil
 	}
-	return nil,nil
+	return nil, nil
 }
 
 const urlKeyword = "Url"
