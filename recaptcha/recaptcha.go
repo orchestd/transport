5fc7c8e4130fc61323ceecb14ce6cede9a228d77
idan <idan.b@heilasystems.com>
2024-01-22 15:42:40 +0200

    POS-8767-pt-install-recapcha

--- recaptcha/recaptcha.go
+++ recaptcha/recaptcha.go
@@ -25,9 +25,9 @@ type SiteVerifyResponse struct {
 }
 
 type GoogleReCapcha struct {
-	conf       configuration.Config
-	cred       credentials.CredentialsGetter
-	httpClient transport.HttpClient
+	recaptchaSecretKey string
+	conf               configuration.Config
+	httpClient         transport.HttpClient
 }
 
 type Recaptcha interface {
@@ -36,30 +36,35 @@ type Recaptcha interface {
 }
 
 func NewRecaptcha(conf configuration.Config, cred credentials.CredentialsGetter, httpClient transport.HttpClient) Recaptcha {
+	recaptchaSecretKey := cred.GetCredentials().RecaptchaKey
+	if recaptchaSecretKey == "" {
+		panic("RECAPTCHA_KEY not found in Credentials")
+	}
+
 	return GoogleReCapcha{
-		conf:       conf,
-		cred:       cred,
-		httpClient: httpClient,
+		conf:               conf,
+		recaptchaSecretKey: recaptchaSecretKey,
+		httpClient:         httpClient,
 	}
 }
 
 func (r GoogleReCapcha) CheckByAction(c context.Context, siteKey, action string) (Status, error) {
-	return r.Check(c, siteKey, action, 0.5)
+	score, err := r.conf.Get("googleReCapchaScore").Float64()
+	if err != nil {
+		return Status{}, err
+	}
+	return r.Check(c, siteKey, action, score)
 }
 
 func (r GoogleReCapcha) Check(c context.Context, siteKey, action string, score float64) (Status, error) {
 	var result Status
-	siteVerifyUrl, err := r.conf.Get("siteverifyurl").String()
+	siteVerifyUrl, err := r.conf.Get("googleReCapchaUrl").String()
 	if err != nil {
 		return result, err
 	}
 
-	recaptchaSecretKey := r.cred.GetCredentials().RecaptchaKey
-	if recaptchaSecretKey == "" {
-		return result, fmt.Errorf("RECAPTCHA_KEY not found in Credentials")
-	}
 	data := map[string]string{
-		"secret":   recaptchaSecretKey,
+		"secret":   r.recaptchaSecretKey,
 		"response": siteKey,
 	}
 	res, err := r.httpClient.PostForm(c, siteVerifyUrl, data, nil)
@@ -69,25 +74,25 @@ func (r GoogleReCapcha) Check(c context.Context, siteKey, action string, score f
 
 	var body SiteVerifyResponse
 	if err := json.Unmarshal(res, &body); err != nil {
-		return result, fmt.Errorf("couldn't decode recaptcha body response", err)
+		return result, fmt.Errorf("Cannot Unmarshal recaptcha body response", err)
 	}
 
 	// Check recaptcha verification success.
 	if !body.Success {
-		result.Error = "unsuccessful recaptcha verify request"
+		result.Error = "Unsuccessful recaptcha verify request"
 		return result, nil
 	}
 
 	// Check response score.
 	if body.Score < score {
-		result.Error = "lower received score than expected"
+		result.Error = fmt.Sprintf("Lower received score (%v) than expected", score)
 		return result, nil
 	}
 
 	// Check response action.
 	if action != "" {
 		if body.Action != action {
-			result.Error = "mismatched recaptcha action"
+			result.Error = fmt.Sprintf("Mismatched recaptcha action %s", action)
 			return result, nil
 		}
 	}
